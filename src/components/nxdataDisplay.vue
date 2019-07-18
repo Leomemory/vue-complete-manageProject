5b0527ed2ac1a56ced3f9d2bd28abbf5b9b354b2
cashbus_lijiang <lijiang.0408@cashbus.com>
2019-07-18 10:16:26 +0800

    countup数字展示，首屏加载动画

---
+++ src/components/nxdataDisplay.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="nxdataDisplay">
+		<el-row :span="24">
+	      <el-col v-for="(item,index) in data" :key="index" :span="span">
+	        <div class="item" :style="{color:color}">
+	          <h5 class="count">
+	          	<nxcount-up :start="14" :end="item.count"></nxcount-up>
+	          </h5>
+	          <span class="splitLine"></span>
+	          <p class="title">{{item.title}}</p>
+	        </div>
+	      </el-col>
+	    </el-row>
+	</div>
+</template>
+
+<script>
+import nxcountUp from '@/components/nxcountUp.vue'
+export default{
+	props: {
+	    option: {
+	      type: Object,
+	      default: () => {}
+	    }
+	},
+	data(){
+		return{
+			data:this.option.data,
+			span: this.option.span || 8,
+			color: this.option.color || 'rgb(63, 161, 255)'
+		}
+	},
+	components:{
+		nxcountUp
+	}
+}
+</script>
+
+<style lang="scss">
+.nxdataDisplay{
+  .item {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 5px 0;
+    text-align: center;
+  }
+  
+  .count {
+    margin: 8px 0;
+    font-weight: bold;
+    font-size: 32px;
+    color: #15A0FF;
+  }
+  
+  .title {
+    color: #999;
+  }
+  
+  .splitLine {
+    display: block;
+    margin: 0 auto;
+    width: 24px;
+    height: 1px;
+    background: #9B9B9B;
+  }
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,43 @@
 <template>
+  <!-- Main page -->
   <div class="page">
-    <h2>Main page</h2>
+    <div class="item">
+      <h4>数据展示</h4>
+      <nxdata-display :option="option"></nxdata-display>
+    </div>
   </div>
 </template>
 
 <script>
+import nxdataDisplay from '@/components/nxdataDisplay.vue'
 export default {
+	data(){
+		return{
+			option: {
+		        span: 8,
+		        color: '#15A0FF',
+		        data: [
+		          {
+		            count: 1000,
+		            title: '日活跃数'
+		          },
+		          {
+		            count: 3000,
+		            title: '月活跃数'
+		          },
+		          {
+		            count: 20000,
+		            title: '年活跃数'
+		          }
+		        ]
+		    }
+		}
+	},
+	components:{
+		nxdataDisplay
+	},
 	methods:{
+		//用来测试axios请求是否有效的方法
 		getUser(){
 			this.$api.user.getUser().then(res=>{
 				console.log('success',res)
@@ -21,6 +52,6 @@ export default {
 }
 </script>
 
-<style>
-	
+<style lang="scss">
+
 </style>
